dec692f4e49d874d9bf8c0603992ce9325767833
Vanshika Choudhary <91586292+vanchica@users.noreply.github.com>
2022-04-16 17:07:10 +0530

    Updated the search Engine of the site

--- src/app.js
+++ src/app.js
@@ -35,8 +35,19 @@ function displayTemperature(response){
   iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
-let apiKey="8fd4039279da65cd9545bf64af9960b5";
-let city = "Delhi";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city){
+  let apiKey="8fd4039279da65cd9545bf64af9960b5";
+  let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
