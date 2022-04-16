9a3933a0466d271e8f88c9849da8fbd712fe982f
Vanshika Choudhary <91586292+vanchica@users.noreply.github.com>
2022-04-16 16:14:03 +0530

    Update the date and time of the site using js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp){
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"];
+  let day = days[date.getDay()];
+  if(hours < 10){
+    hours = `0${hours}`;
+  }
+  if(minutes < 10){
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}  ${hours}:${minutes}`;
+  
+}
+
 function displayTemperature(response){
   console.log(response.data.main.temp);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,14 +22,17 @@ function displayTemperature(response){
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey="8fd4039279da65cd9545bf64af9960b5";
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=Delhi&appid=${apiKey}&units=metric`;
+let city = "Delhi";
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
